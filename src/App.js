8b3948ff30083f6c0d1fd20c39b5b4207646418c
legarrod <luis.garcia@atlanticsoft.us>
2022-04-13 22:42:53 -0500

    layout creado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import About from './About';
 import Home from './Home';
+import Layout from './components/Layout/Layout';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <nav>
+      <Layout>
+        <Routes>
+          <Route path="/about" element={<About />} />
+
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+      {/* <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -15,15 +23,10 @@ function App() {
             <Link to="/about">About</Link>
           </li>
         </ul>
-      </nav>
+      </nav> */}
 
       {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-      <Routes>
-        <Route path="/about" render={<About />} />
-
-        <Route path="/" render={<Home />} />
-      </Routes>
     </Router>
   );
 }
