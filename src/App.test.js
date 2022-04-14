415a85d437be6c8918a9534a6790a88d71250a8f
legarrod <luis.garcia@atlanticsoft.us>
2022-04-13 22:06:17 -0500

    primer configuraciojn

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { sumando } from './suma';
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(sumando(2, 4)).toEqual(3);
 });
